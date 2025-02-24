904013fa29de0f8a80965820fc15dec127b66fb9
kasakj <kubakasak@seznam.cz>
2025-02-24 14:10:39 +0100

    Fixed Id and added method and class

--- src/main/java/com/example/gaming/HelloController.java
+++ src/main/java/com/example/gaming/HelloController.java
@@ -15,16 +15,6 @@ import java.io.IOException;
 public class HelloController {
     @FXML
     private ListView<Player> Players = new ListView<Player>();
-    @FXML
-    private TextField name;
-    @FXML
-    private TextField level;
-    @FXML
-    private TextField playerClass;
-    @FXML
-    private TextField email;
-    @FXML
-    private TextField registrationDate;
 
     @FXML
     private void CreatePlayer() throws IOException {
@@ -46,54 +36,12 @@ public class HelloController {
     private void DeletePlayer(){
         Players.getItems().remove(Players.getSelectionModel().getSelectedItem());
     }
-    @FXML
-    private void EditPlayer(){
-        String newName = "";
-        int newLevel = 0;
-        String newPlayerClass = "";
-        String newEmail = "";
-        String newRegistrationDate = "";
 
-        int index = Players.getSelectionModel().getSelectedIndex();
-
-        if (name.getText().isEmpty()){
-            newName = Players.getSelectionModel().getSelectedItem().name;
-        }
-        else { newName = name.getText(); }
-
-        if (level.getText().isEmpty()){
-            newLevel = Players.getSelectionModel().getSelectedItem().level;
-        }
-        else { newLevel = Integer.parseInt(level.getText()); }
-
-        if (playerClass.getText().isEmpty()){
-            newPlayerClass = Players.getSelectionModel().getSelectedItem().playerClass;
-        }
-        else { newPlayerClass = playerClass.getText(); }
-
-        if (email.getText().isEmpty()){
-            newEmail = Players.getSelectionModel().getSelectedItem().email;
-        }
-        else { newEmail = email.getText(); }
-
-        if (registrationDate.getText().isEmpty()){
-            newRegistrationDate = Players.getSelectionModel().getSelectedItem().registrationDate;
-        }
-        else { newRegistrationDate = registrationDate.getText(); }
-
-        Players.getItems().set(index, new Player(newName, newLevel, newPlayerClass, newEmail, newRegistrationDate));
-    }
     @FXML
     private void ShowPlayer(){
         // TODO: Implement this
     }
 
-    private void makeMockData(){
-        for (int i = 0; i < 5; i++) {
-            Players.getItems().add(new Player(i+"name", i, i+"class", i+"@"+i+".cz", "2021.03.04"));
-            Players.refresh();
-        }
-    }
     public void initialize(){
         Players.getItems().add(new Player("Kaiy", 100,"Mág", "jibladfgjknb@seznam"+".cz", "2021.03.04"));
         Players.getItems().add(new Player("Karel", 10,"Bojovník", "oikjadfgjio@email"+".cz", "2021.08.04"));
